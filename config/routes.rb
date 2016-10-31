2f7fb7400dd39eec762f343574b5d589b53bb68c
kushrohra <rohrakush@gmail.com>
2016-10-31 18:39:02 +0000

    added sass gem bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
+
+  #below cmd will help you to customize your URL
+  #get 'about', to: 'pages#aboutus'
+  
+  get 'pages/aboutus'
+
+  get 'pages/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
